d4f42f7adb63f334562bbde051ceb6883701ae47
李杰 <2624824150@qq.com>
2018-08-20 19:32:41 +0800

    update

--- config-server/src/main/java/com/jl/demo/ConfigApplication.java
+++ config-server/src/main/java/com/jl/demo/ConfigApplication.java
@@ -3,9 +3,13 @@ package com.jl.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class);
