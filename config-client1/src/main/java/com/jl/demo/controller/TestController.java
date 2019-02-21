09aaf99d6a1c89ee689d0c61afe43933930588a9
李杰 <2624824150@qq.com>
2019-02-21 16:41:15 +0800

    代码提交

--- config-client1/src/main/java/com/jl/demo/controller/TestController.java
+++ config-client1/src/main/java/com/jl/demo/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.jl.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 public class TestController {
-    @Value("${name}")
-    String name;
+//    @Value("${myname}")
+//    String name;
 
-    @Value("${version}")
-    String version;
+    @Autowired
+    private Environment environment;
+
+    @Value("${spring.application.name}")
+    String applicationName;
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
     public String hi(){
+        String name =  environment.getProperty("myname");
         return name;
     }
 }
